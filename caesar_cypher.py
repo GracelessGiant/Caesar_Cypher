1b461d95c59f00ab53e1770c28f9e2df621084e2
GracelessGiant <marchionepat43@gmail.com>
2020-06-18 22:21:57 -0400

    Added the Dictionary CSV and created the dictionary set

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -1,1 +1,23 @@
+"""
+Creator: GracelessGiant
+Peep my twitter [@GracelessGiant]
+Description: Caesar_Cypher offers two functions.
+    1. Enter a word/phase and amount to create cyphered word/phase
+    2. Enter a cyphered work/phase to get potential solutions
+"""
+dictionary = set()
 
+
+def create_set():
+    with open("dictionary.csv") as file:
+        for line in file:
+            word = line.strip().split(",")[0]
+            dictionary.add(word)
+
+
+def main():
+    create_set()
+
+
+if __name__ == '__main__':
+    main()
