f31c850892c45b5e62a5b3b5a6be2d3f5bcb811e
GracelessGiant <marchionepat43@gmail.com>
2020-06-18 22:50:31 -0400

    Added loop back through terminal and stubs for create_cypher() and decypher()

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -5,6 +5,9 @@ Description: Caesar_Cypher offers two functions.
     1. Enter a word/phase and amount to create cyphered word/phase
     2. Enter a cyphered work/phase to get potential solutions
 """
+
+import os
+
 dictionary = set()
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
             "v", "w", "x", "y", "z"]
@@ -17,16 +20,26 @@ def create_set():
             dictionary.add(word)
 
 
+def create_cypher():
+    pass
+
+
+def decypher():
+    pass
+
+
 def main():
     create_set()
     function = int(input("Enter the associated number with the desired function:\n1. Apply desired Caesar cypher to "
-                         "original word/phase\n2. Decpyher text to find original word/phase\n"))
+                         "original word/phase\n2. Decpyher text to find original word/phase\nFunction: "))
     if function == 1:
-        print("Create cypher")
+        create_cypher()
     elif function == 2:
-        print("Decypher")
+        decypher()
     else:
         print("That's not a correct input. Enter again.")
+        os.system("clear")
+        main()
 
 
 if __name__ == '__main__':
