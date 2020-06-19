b00ee955b6201c29c1a0d607c94e29815295287e
GracelessGiant <marchionepat43@gmail.com>
2020-06-18 22:44:31 -0400

    Added basic input prompts and alphabetical list

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -6,6 +6,8 @@ Description: Caesar_Cypher offers two functions.
     2. Enter a cyphered work/phase to get potential solutions
 """
 dictionary = set()
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+            "v", "w", "x", "y", "z"]
 
 
 def create_set():
@@ -17,6 +19,14 @@ def create_set():
 
 def main():
     create_set()
+    function = int(input("Enter the associated number with the desired function:\n1. Apply desired Caesar cypher to "
+                         "original word/phase\n2. Decpyher text to find original word/phase\n"))
+    if function == 1:
+        print("Create cypher")
+    elif function == 2:
+        print("Decypher")
+    else:
+        print("That's not a correct input. Enter again.")
 
 
 if __name__ == '__main__':
